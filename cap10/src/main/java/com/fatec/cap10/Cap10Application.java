87c0b0fde6b848b906d27e59f33a413ceac87234
Robsonhf <robson.hferreira@hotmail.com>
2021-06-18 20:59:26 -0300

    CAP10 - Step1: Criação de caso de teste que não compila

---
+++ cap10/src/main/java/com/fatec/cap10/Cap10Application.java
@@ -0,0 +1,13 @@
+package com.fatec.cap10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cap10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cap10Application.class, args);
+	}
+
+}

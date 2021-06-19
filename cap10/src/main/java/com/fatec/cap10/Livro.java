34a3f3c9076e0c370f88ca344a6919c27ab6b9b2
Robsonhf <robson.hferreira@hotmail.com>
2021-06-18 21:13:58 -0300

    CAP10 Step4: faz o teste passar (baby step) para a validação do estado atual

--- cap10/src/main/java/com/fatec/cap10/Livro.java
+++ cap10/src/main/java/com/fatec/cap10/Livro.java
@@ -1,5 +1,45 @@
 package com.fatec.cap10;
 
 public class Livro {
+	String isbn;
+	String titulo;
+	String autor;
 
+	public Livro() {
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public String setIsbn(String isbn) {
+		String mensagem = null;
+		if (isbn.equals("")) {
+			mensagem = "Informações inválidas";
+		} else {
+			this.isbn = isbn;
+		}
+		return mensagem;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return true;
+	}
 }
